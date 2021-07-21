4a14b63e04d72152a104f09317feeaf893333653
Kevin Zheng <4399kevin11@gmail.com>
2021-07-21 20:31:59 +0800

    full

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+let Footer = () => {
+  return (
+    <div className="mt-5 ">
+      <div className="py-5">
+        <div className="border-t border-gray-200"></div>
+      </div>
+      <div className="py-3-safe text-xs text-gray-400 text-center">
+        <p>Kevin Zheng © 2021.</p>
+      </div>
+    </div>
+  )
+}
+
+
+
+export default Footer
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,17 +4,14 @@ import CSS from 'csstype';
 import logo from '../logo.png'
 
 // components
-import Spinner from './Spinner'
+import { Spinner, Footer } from '../components'
 
 
 
-type StateType = {
-  link: String
-}
-
-type apiResultType = {
+interface apiResultType {
+  message?: String,
   ip: String,
-  success: String,
+  success: boolean,
   type: String,
   continent: String,
   continent_code: String,
@@ -50,33 +47,39 @@ const maxWidth: CSS.Properties = {
 
 
 
-const IndexPage = (initialState: StateType, ipResult: apiResultType) => {
-  
-  let [values, setValues] = useState(initialState);
+const IndexPage = ({}, ipResult: apiResultType) => {
+  let [searchString, setSearchString] = useState(String);
   let [resultInfo, setResultInfo] = useState(ipResult);
   let [isLaoding, setLoading] = useState(true);
-
+  let [cacheSearchString, setCacheSearchString] = useState(String);
 
   // onChange
   let onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setValues({ ...values, [event.target.name]: event.target.value });
+    setSearchString(event.target.value)
+      //setValues({ ...values, [event.target.name]: event.target.value });
   };
 
-  let onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  let onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    
-    if(values.link !== undefined && values.link.length !== 0) {
+
+    if(searchString !== undefined && searchString.length !== 0) {
       setLoading(true)
 
-      axios.get(`http://ipwhois.app/json/${values.link}?lang=en`)
+      // request 
+      await axios.get(`http://ipwhois.app/json/${searchString}?lang=en`)
       .then((res) => {
         let resultInfo = res.data
         setResultInfo(resultInfo)
+        setCacheSearchString(searchString.toString())
       })
       .catch(() => {
       }).finally(() => setLoading(false))
+      //if(cacheSearchString !== searchString) {
+      //}
     } 
   };
+
+
 
   // componentDidMount
   useEffect(() => {
@@ -100,125 +103,135 @@ const IndexPage = (initialState: StateType, ipResult: apiResultType) => {
   if(isLaoding) {
     resultCard = '';
   } else {
-
-    resultCard = 
-    <div className="mt-10 p-3 border bg-white rounded-xl shadow-xl">
-      <div className="px-4 py-5">
-        <div className="font-bold text-green-900 text-3xl">一般資訊</div>
-
-          <div className="mt-3 grid grid-cols-1 sm:grid-cols-12">
-            <div className="col-span-6">
-              <div className="text-lg font-bold text-green-900">IP 位址</div>
-              <div className="text-2xl font-bold text-red-800">{ resultInfo.ip }</div>
-            </div>
-
-            <div className="col-span-6">
-              <div className="text-lg font-bold text-green-900">網際協定版本</div>
-              <div className="text-2xl font-bold text-red-800">{ resultInfo.type }</div>
-            </div>
+    if(resultInfo.success === false) {
+      
+      resultCard = 
+        <div
+          className="mt-10 p-3 border border-red-300 bg-red-100 rounded-xl shadow-xl"
+        >
+          <div className="px-4 py-5">
+            <div className="font-bold text-red-900 text-2xl">查詢錯誤</div>
+            <p className="mt-4">{ resultInfo.message }</p>
           </div>
+        </div>;
 
+    } else {
+      resultCard = 
+        <div className="mt-10 p-3 border bg-white rounded-xl shadow-xl">
+          <div className="px-4 py-5">
+            <div className="font-bold text-green-900 text-3xl">一般資訊</div>
 
+              <div className="mt-3 grid grid-cols-1 sm:grid-cols-12">
+                <div className="col-span-6">
+                  <div className="text-lg font-bold text-green-900">IP 位址</div>
+                  <div className="text-2xl font-bold text-red-800">{ resultInfo.ip }</div>
+                </div>
 
-
-          <div className="mt-10 font-bold text-green-900 text-3xl">地理位置</div>
-
-          <div className="mt-3 grid grid-cols-1 sm:grid-cols-12">
-            <div className="col-span-6">
-              <div className="text-lg font-bold text-green-900">緯度</div>
-              <div className="text-2xl font-bold text-red-800">{ resultInfo.latitude }</div>
-            </div>
-
-            <div className="col-span-6">
-              <div className="text-lg font-bold text-green-900">經度</div>
-              <div className="text-2xl font-bold text-red-800">{ resultInfo.longitude }</div>
-            </div>
-          </div>
-
-          <div className="mt-5 grid grid-cols-1 sm:grid-cols-12">
-            <div className="col-span-4">
-              <div className="text-lg font-bold text-green-900">地區</div>
-              <div className="text-2xl font-bold text-red-800">
-                <img className="rounded-md" src={resultInfo.country_flag.toString()} width="68" height="48" alt="region_flag" />
-                { resultInfo.country }, { resultInfo.country_code }
+                <div className="col-span-6">
+                  <div className="text-lg font-bold text-green-900">網際協定版本</div>
+                  <div className="text-2xl font-bold text-red-800">{ resultInfo.type }</div>
+                </div>
               </div>
-            </div>
 
-            <div className="col-span-4">
-              <div className="text-lg font-bold text-green-900">城市</div>
-              <div className="text-2xl font-bold text-red-800">{ resultInfo.city }</div>
-            </div>
 
-            <div className="col-span-4">
-              <div className="text-lg font-bold text-green-900">國際電話區號</div>
-              <div className="text-2xl font-bold text-red-800">{ resultInfo.country_phone }</div>
-            </div>
+              <div className="mt-10 font-bold text-green-900 text-3xl">地理位置</div>
+
+              <div className="mt-3 grid grid-cols-1 sm:grid-cols-12">
+                <div className="col-span-6">
+                  <div className="text-lg font-bold text-green-900">緯度</div>
+                  <div className="text-2xl font-bold text-red-800">{ resultInfo.latitude }</div>
+                </div>
+
+                <div className="col-span-6">
+                  <div className="text-lg font-bold text-green-900">經度</div>
+                  <div className="text-2xl font-bold text-red-800">{ resultInfo.longitude }</div>
+                </div>
+              </div>
+
+              <div className="mt-5 grid grid-cols-1 sm:grid-cols-12">
+                <div className="col-span-4">
+                  <div className="text-lg font-bold text-green-900">地區</div>
+                  <div className="text-2xl font-bold text-red-800">
+                    <img className="rounded-md" src={resultInfo.country_flag.toString()} width="68" height="48" alt="region_flag" />
+                    { resultInfo.country }, { resultInfo.country_code }
+                  </div>
+                </div>
+
+                <div className="col-span-4">
+                  <div className="text-lg font-bold text-green-900">城市</div>
+                  <div className="text-2xl font-bold text-red-800">{ resultInfo.city }</div>
+                </div>
+
+                <div className="col-span-4">
+                  <div className="text-lg font-bold text-green-900">國際電話區號</div>
+                  <div className="text-2xl font-bold text-red-800">{ resultInfo.country_phone }</div>
+                </div>
+              </div>
+
+
+
+              <div className="mt-10 font-bold text-green-900 text-3xl">時區資訊</div>
+
+              <div className="mt-3 text-lg font-bold text-green-900">時區</div>
+              <div className="text-2xl font-bold text-red-800">{ resultInfo.timezone }</div>
+
+              <div className="mt-5 text-lg font-bold text-green-900">時區名稱</div>
+              <div className="text-2xl font-bold text-red-800">{ resultInfo.timezone_name }</div>
+
+              <div className="mt-5 grid grid-cols-1 sm:grid-cols-12">
+                <div className="col-span-6">
+                  <div className="text-lg font-bold text-green-900">GMT 時區</div>
+                  <div className="text-2xl font-bold text-red-800">{ resultInfo.timezone_gmt }</div>
+                </div>
+
+                <div className="col-span-6">
+                  <div className="text-lg font-bold text-green-900">GMT 時區偏移</div>
+                  <div className="text-2xl font-bold text-red-800">{ resultInfo.timezone_gmtOffset }</div>
+                </div>
+              </div>
+
+
+
+              <div className="mt-10 font-bold text-green-900 text-3xl">ISP 資訊</div>
+        
+              <div className="mt-5 text-lg font-bold text-green-900">ASN</div>
+              <div className="text-2xl font-bold text-red-800">{ resultInfo.asn }</div>
+
+              <div className="mt-5 text-lg font-bold text-green-900">ORG</div>
+              <div className="text-2xl font-bold text-red-800">{ resultInfo.org }</div>
+              
+              <div className="mt-5 text-lg font-bold text-green-900">網路供應商</div>
+              <div className="text-2xl font-bold text-red-800">{ resultInfo.isp }</div>
+              
+              
+              
+              <div className="mt-10 font-bold text-green-900 text-3xl">貨幣資訊</div>
+
+              <div className="mt-5 grid grid-cols-1 sm:grid-cols-12">
+                <div className="col-span-4">
+                  <div className="text-lg font-bold text-green-900">貨幣</div>
+                  <div className="text-2xl font-bold text-red-800">{ resultInfo.currency }</div>
+                </div>
+
+                <div className="col-span-4">
+                  <div className="text-lg font-bold text-green-900">貨幣代碼</div>
+                  <div className="text-2xl font-bold text-red-800">{ resultInfo.currency_code }</div>
+                </div>
+
+                <div className="col-span-4">
+                  <div className="text-lg font-bold text-green-900">貨幣符號</div>
+                  <div className="text-2xl font-bold text-red-800">{ resultInfo.currency_symbol }</div>
+                </div>
+              </div>
+
+              <div className="mt-5 text-lg font-bold text-green-900">匯率 USD to</div>
+              <div className="text-2xl font-bold text-red-800">{ resultInfo.currency_rates }</div>
+
+              <div className="mt-10 text-lg font-bold text-green-900">已完成的查詢次數</div>
+              <div className="text-2xl font-bold text-red-800">{ resultInfo.completed_requests }</div>
           </div>
-
-
-
-          <div className="mt-10 font-bold text-green-900 text-3xl">時區資訊</div>
-
-          <div className="mt-3 text-lg font-bold text-green-900">時區</div>
-          <div className="text-2xl font-bold text-red-800">{ resultInfo.timezone }</div>
-
-          <div className="mt-5 text-lg font-bold text-green-900">時區名稱</div>
-          <div className="text-2xl font-bold text-red-800">{ resultInfo.timezone_name }</div>
-
-          <div className="mt-5 grid grid-cols-1 sm:grid-cols-12">
-            <div className="col-span-6">
-              <div className="text-lg font-bold text-green-900">GMT 時區</div>
-              <div className="text-2xl font-bold text-red-800">{ resultInfo.timezone_gmt }</div>
-            </div>
-
-            <div className="col-span-6">
-              <div className="text-lg font-bold text-green-900">GMT 時區偏移</div>
-              <div className="text-2xl font-bold text-red-800">{ resultInfo.timezone_gmtOffset }</div>
-            </div>
-          </div>
-
-
-
-          <div className="mt-10 font-bold text-green-900 text-3xl">ISP 資訊</div>
-    
-          <div className="mt-5 text-lg font-bold text-green-900">ASN</div>
-          <div className="text-2xl font-bold text-red-800">{ resultInfo.asn }</div>
-
-          <div className="mt-5 text-lg font-bold text-green-900">ORG</div>
-          <div className="text-2xl font-bold text-red-800">{ resultInfo.org }</div>
-          
-          <div className="mt-5 text-lg font-bold text-green-900">網路供應商</div>
-          <div className="text-2xl font-bold text-red-800">{ resultInfo.isp }</div>
-          
-          
-          
-          <div className="mt-10 font-bold text-green-900 text-3xl">貨幣資訊</div>
-
-          <div className="mt-5 grid grid-cols-1 sm:grid-cols-12">
-            <div className="col-span-4">
-              <div className="text-lg font-bold text-green-900">貨幣</div>
-              <div className="text-2xl font-bold text-red-800">{ resultInfo.currency }</div>
-            </div>
-
-            <div className="col-span-4">
-              <div className="text-lg font-bold text-green-900">貨幣代碼</div>
-              <div className="text-2xl font-bold text-red-800">{ resultInfo.currency_code }</div>
-            </div>
-
-            <div className="col-span-4">
-              <div className="text-lg font-bold text-green-900">貨幣符號</div>
-              <div className="text-2xl font-bold text-red-800">{ resultInfo.currency_symbol }</div>
-            </div>
-          </div>
-
-          <div className="mt-5 text-lg font-bold text-green-900">匯率 USD to</div>
-          <div className="text-2xl font-bold text-red-800">{ resultInfo.currency_rates }</div>
-
-          <div className="mt-10 text-lg font-bold text-green-900">已完成的查詢次數</div>
-          <div className="text-2xl font-bold text-red-800">{ resultInfo.completed_requests }</div>
-      </div>
-    </div>;
-
+        </div>;
+    }
   }
 
 
@@ -246,8 +259,8 @@ const IndexPage = (initialState: StateType, ipResult: apiResultType) => {
               <input
                 className="mt-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                 type="text"
-                name="link"
-                id="link"
+                name="searchString"
+                id="searchString"
                 autoComplete="off"
                 placeholder="貼上 IP 位址，例如： 216.58.200.46"
                 onChange={onChange}
@@ -255,7 +268,7 @@ const IndexPage = (initialState: StateType, ipResult: apiResultType) => {
             </div>
 
             <p className="mt-2 text-sm text-gray-500">
-              現在查詢的 IP 是：
+              現在查詢的 IP 是： { cacheSearchString }
             </p>
 
             <p className="text-sm text-gray-500">
@@ -266,18 +279,18 @@ const IndexPage = (initialState: StateType, ipResult: apiResultType) => {
         {
           // button
         }
-        <div className="mt-5">
-          <button
-            className="w-full flex flex-row justify-center items-center p-3 bg-indigo-900 text-white rounded-lg shadow-lg focus:outline-none active:bg-indigo-800 disabled:opacity-80"
-            type="submit"
-          >
-            { isLaoding ? <Spinner/> : <span>查詢</span>}
-          </button>
-        </div>
+        <button
+          className="mt-5 w-full flex flex-row justify-center items-center p-3 bg-indigo-900 text-white rounded-lg shadow-lg focus:outline-none active:bg-indigo-800 disabled:opacity-80"
+          type="submit"
+        >
+          { isLaoding ? <Spinner/> : <span>查詢</span>}
+        </button>
+  
       </form>
       
       { resultCard }
-
+      
+      <Footer/>
     </div>
   )
 }
