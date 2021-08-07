7a4cc0fb6a2a2622d61915589ba6d9e847d63d74
Snow FireWolf <4399kevin11@gmail.com>
2021-08-08 02:36:55 +0800

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import st from '../css/tailwind.module.css'
 
 
 let Footer = () => {
   return (
-    <div className="mt-5 ">
-      <div className="py-5">
-        <div className="border-t border-gray-200"></div>
+    <div className={st['mt-5']}>
+      <div className={st['py-5']}>
+        <div className={`${st['border-t']} ${st['border-gray-200']}`}></div>
       </div>
-      <div className="py-3-safe text-xs text-gray-400 text-center">
+      <div className={`${st['py-3-safe']} ${st['text-xs']} ${st['text-gray-400']} ${st['text-center']}`}>
         <p>Kevin Zheng © 2021.</p>
       </div>
     </div>
