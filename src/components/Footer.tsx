c0dffc2ed17bfa937b03e1a579d5067c050ff0e8
Snow FireWolf <4399kevin11@gmail.com>
2021-08-08 02:39:58 +0800

    undefined styles fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ let Footer = () => {
       <div className={st['py-5']}>
         <div className={`${st['border-t']} ${st['border-gray-200']}`}></div>
       </div>
-      <div className={`${st['py-3-safe']} ${st['text-xs']} ${st['text-gray-400']} ${st['text-center']}`}>
+      <div className={`${st['py-3']} ${st['text-xs']} ${st['text-gray-400']} ${st['text-center']}`}>
         <p>Kevin Zheng © 2021.</p>
       </div>
     </div>
